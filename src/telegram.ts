abad205ce4c41c2b4be4d32bdeab318ceee6adbc
yeongbin-gong <youngbin816@gmail.com>
2023-01-19 17:11:19 +0900

    node-schedule 적용

--- src/telegram.ts
+++ src/telegram.ts
@@ -1,73 +1,72 @@
 import moment from 'moment-timezone';
+import schedule from 'node-schedule';
 import TelegramBot from 'node-telegram-bot-api';
 import c_raw from './schema/c_raw';
 
-const now = moment
-  .tz('Asia/Seoul')
-  .startOf('minute')
-  .format('YYYY-MM-DD hh:mm:ss');
-const beforeOneMinute = moment
-  .tz('Asia/Seoul')
-  .subtract(1, 'minute')
-  .startOf('minute')
-  .format('YYYY-MM-DD hh:mm:ss');
-
-const token: string = process.env.TELEGRAM_TOKEN!;
-const chat_id: string = process.env.TELEGRAM_CHATID!;
-
-const bot: TelegramBot = new TelegramBot(token, { polling: true });
-
-async function findByCtime() {
-  await c_raw.aggregate([
-    {
-      $match: {
-        c_time: {
-          $gte: moment(beforeOneMinute).toISOString(true),
-          $lt: moment(now).toISOString(true),
-        },
-      },
-    },
-    {
-      $group: {
-        _id: '$code',
-      },
-    },
-  ]);
-}
-export default async (): Promise<void> => {
+async function checkedMoreThanFiveBillion(): Promise<
+  { _id: string; theSumOfTheMinutes: number }[]
+> {
   const date = new Date();
   const offset = date.getTimezoneOffset() * 60000;
   const dateOffset = new Date(date.getTime() - offset);
-  const startD = moment(dateOffset)
+  const startMinute = moment(dateOffset)
     .tz('Asia/Seoul')
     .subtract(1, 'minute')
     .startOf('minute')
     .toISOString();
-  const endD = moment(dateOffset)
+  const endMinute = moment(dateOffset)
     .tz('Asia/Seoul')
     .startOf('minute')
     .toISOString();
 
-  const test: any = await c_raw.aggregate([
+  return await c_raw.aggregate([
     {
       $match: {
         c_time: {
-          $gte: new Date('2023-01-18T10:00:00.000Z'),
-          $lt: new Date('2023-01-18T15:30:00.000Z'),
+          $gte: new Date(startMinute),
+          $lt: new Date(endMinute),
         },
       },
     },
     {
       $group: {
         _id: '$code',
-        total: { $sum: { $multiply: ['$c_price', '$c_volume'] } },
+        theSumOfTheMinutes: { $sum: { $multiply: ['$c_price', '$c_volume'] } },
       },
     },
     {
-      $sort: { total: -1 },
+      $match: {
+        theSumOfTheMinutes: { $gte: 5000000000 },
+      },
+    },
+    {
+      $sort: { theSumOfTheMinutes: -1 },
     },
   ]);
-  console.log(test);
-  console.log(test.length);
-  //   await bot.sendMessage(chat_id, 'test message');
-};
+}
+
+async function sendTelegramMessages(code: string, message: string) {
+  const token: string = process.env.TELEGRAM_TOKEN!;
+  const chat_id: string = process.env.TELEGRAM_CHATID!;
+  const bot: TelegramBot = new TelegramBot(token, { polling: true });
+
+  await bot.sendMessage(chat_id, `${code} : ${message}`);
+}
+
+// function createingTelegramMessages(
+//   data: { _id: string; theSumOfTheMinutes: number }[]
+// ) {}
+
+export default function main() {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+  schedule.scheduleJob('0 */1 8-18 * * 1-6 ', async () => {
+    const checkedMoreThanFiveBillionData: {
+      _id: string;
+      theSumOfTheMinutes: number;
+    }[] = await checkedMoreThanFiveBillion();
+
+    if (!checkedMoreThanFiveBillionData.length) return;
+
+    // createingTelegramMessages(checkedMoreThanFiveBillionData);
+  });
+}
