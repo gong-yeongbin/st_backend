c4da7ab88068c519a1eb21cda24f7f81f93e3c70
yeongbin-gong <youngbin816@gmail.com>
2023-02-12 22:58:17 +0900

    스케쥴 시간 수정

--- src/telegram.ts
+++ src/telegram.ts
@@ -19,6 +19,8 @@ async function sendTelegramMessages(
 function fnCreateingTelegramMessages(
   data: { code: string; price: number }[]
 ): string {
+  if (!data.length) return '';
+
   let arrayMessage: Array<string>;
   arrayMessage = [''];
 
@@ -30,8 +32,7 @@ function fnCreateingTelegramMessages(
 }
 export default function main() {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-  // schedule.scheduleJob('0 */1 8-18 * * 1-7 ', async () => {
-  schedule.scheduleJob('*/30 * 8-23 * * 1-7 ', async () => {
+  schedule.scheduleJob('0 */1 8-18 * * 1-7 ', async () => {
     await sendTelegramMessages(
       '50억 이상',
       fnCreateingTelegramMessages(await fnCheckedMoreThanFiveBillion())
