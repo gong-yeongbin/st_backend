cc73732b8ee91f3cc134b5f1a047cd6bb53b3c7c
yeongbin-gong <youngbin816@gmail.com>
2023-02-13 08:25:56 +0900

    텔레그렘 메세지전송 분기처리

--- src/telegram.ts
+++ src/telegram.ts
@@ -1,5 +1,6 @@
 import schedule from 'node-schedule';
 import TelegramBot from 'node-telegram-bot-api';
+import { cRawReturn } from './interfaces/cRaw';
 import {
   fnaNetPurchaseOfThePreviousDayMoreThan10BillionWon,
   fnCheckedMoreThanFiveBillion,
@@ -33,16 +34,28 @@ function fnCreateingTelegramMessages(
 export default function main() {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
   schedule.scheduleJob('0 */1 8-18 * * 1-7 ', async () => {
-    await sendTelegramMessages(
-      '50억 이상',
-      fnCreateingTelegramMessages(await fnCheckedMoreThanFiveBillion())
-    );
-    await sendTelegramMessages(
-      '전일 순매수 100억 이상',
-      fnCreateingTelegramMessages(
-        await fnaNetPurchaseOfThePreviousDayMoreThan10BillionWon()
-      )
-    );
+    const checkedMoreThanFiveBillionData: cRawReturn[] =
+      await fnCheckedMoreThanFiveBillion();
+
+    if (checkedMoreThanFiveBillionData.length > 0) {
+      await sendTelegramMessages(
+        '50억 이상',
+        fnCreateingTelegramMessages(checkedMoreThanFiveBillionData)
+      );
+    }
+
+    const netPurchaseOfThePreviousDayMoreThan10BillionWonData: cRawReturn[] =
+      await fnaNetPurchaseOfThePreviousDayMoreThan10BillionWon();
+
+    if (checkedMoreThanFiveBillionData.length > 0) {
+      await sendTelegramMessages(
+        '전일 순매수 100억 이상',
+        fnCreateingTelegramMessages(
+          netPurchaseOfThePreviousDayMoreThan10BillionWonData
+        )
+      );
+    }
+
     // await fnaNetPurchaseOfThePreviousDayMoreThan10BillionWon();
     // if (!checkedMoreThanFiveBillionData.length) return;
     // void sendTelegramMessages(
