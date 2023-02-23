6c3a7f9cdc134cd6767d438fa34d61f81eebb952
yeongbin-gong <youngbin816@gmail.com>
2023-02-23 17:05:14 +0900

    controller 리턴 데이터 타입 수정

--- src/controllers/stork.ts
+++ src/controllers/stork.ts
@@ -27,7 +27,7 @@ const storkController = {
       const moreThan15percentComparedToThePreviousDayData: cRawReturn[] =
         await storkService.moreThan15percentComparedToThePreviousDay();
 
-      return res.json({ data: moreThan15percentComparedToThePreviousDayData });
+      return res.json(moreThan15percentComparedToThePreviousDayData);
     } catch (error) {
       console.log(error);
     }
@@ -52,7 +52,7 @@ const storkController = {
       const checkedMoreThanFiveBillionData: cRawReturn[] =
         await storkService.checkedMoreThanFiveBillion();
 
-      return res.json({ data: checkedMoreThanFiveBillionData });
+      return res.json(checkedMoreThanFiveBillionData);
     } catch (error) {
       console.log(error);
     }
