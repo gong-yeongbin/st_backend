c02dc15e3d6a9f25db5cdf284bd48b491738e1b8
yeongbin-gong <youngbin816@gmail.com>
2023-02-26 02:32:46 +0900

    redis test

--- src/loaders/redis.ts
+++ src/loaders/redis.ts
@@ -3,9 +3,11 @@ import config from '../configs';
 
 export default (async function () {
   const redisInfo = {
-    host: config.redis_host,
-    port: config.redis_port,
-    legacyMode: true,
+    socket: {
+      host: config.redis_host,
+      port: config.redis_port,
+      legacyMode: true,
+    },
   };
   const client = redis.createClient(redisInfo);
 
