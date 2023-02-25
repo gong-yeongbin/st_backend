ac084e090e436744eb34d5017cd4965238fe8106
yeongbin-gong <youngbin816@gmail.com>
2023-02-26 02:54:40 +0900

    redis connect test

--- src/loaders/redis.ts
+++ src/loaders/redis.ts
@@ -2,14 +2,10 @@ import * as redis from 'redis';
 import config from '../configs';
 
 export default (async function () {
-  const redisInfo = {
-    socket: {
-      host: config.redis_host,
-      port: config.redis_port,
-      legacyMode: true,
-    },
-  };
-  const client = redis.createClient();
-  await client.connect();
-  await client.set('connect', 'success');
+  try {
+    const client = redis.createClient();
+    await client.connect().then(() => console.log('connect success!!!'));
+  } catch (error) {
+    console.error(error);
+  }
 })();
