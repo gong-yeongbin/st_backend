8afaebaf502a59f855514c953ac7ecf93778b09d
yeongbin-gong <youngbin816@gmail.com>
2023-02-26 15:30:42 +0900

    dotenv error 수정

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,13 +1,12 @@
 import dotenv from 'dotenv';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'local';
+console.log(process.env.NODE_ENV);
 
-if (process.env.NODE_ENV === 'local') {
-  const envFound = dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+const envFound = dotenv.config();
 
-  if (envFound.error) {
-    throw new Error("⚠️  Couldn't find .env file  ⚠️");
-  }
+if (envFound.error) {
+  throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
 
 export default {
