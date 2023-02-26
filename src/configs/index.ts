d30429cf75285c64e1d213c455123031c4997cc1
yeongbin-gong <youngbin816@gmail.com>
2023-02-26 15:45:07 +0900

    dotenv multiple env

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,9 +1,6 @@
 import dotenv from 'dotenv';
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'local';
-console.log(process.env.NODE_ENV);
-
-const envFound = dotenv.config();
+const envFound = dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 if (envFound.error) {
   throw new Error("⚠️  Couldn't find .env file  ⚠️");
