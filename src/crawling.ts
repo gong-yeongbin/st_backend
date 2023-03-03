238487b7b78d3e588c8572dd3948d1eede2431dc
yeongbin-gong <youngbin816@gmail.com>
2023-03-04 00:26:06 +0900

    puppeteer test

--- src/crawling.ts
+++ src/crawling.ts
@@ -7,7 +7,7 @@ import storkService from './services/stork';
 import { startDate } from './util/date';
 
 (function () {
-  schedule.scheduleJob('0 5 0 * * * ', async () => {
+  schedule.scheduleJob('0 35 0 * * * ', async () => {
     // schedule.scheduleJob('0 5 0 * * 1-7 ', async () => {
     console.log('crawling');
     const mRawList: ImRaw[] = await storkService.getMrawList();
