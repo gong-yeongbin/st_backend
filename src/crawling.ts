afbc55457176b1d42e3445d8013f07d27f81c222
yeongbin-gong <youngbin816@gmail.com>
2023-03-03 23:59:58 +0900

    puppeteer error

--- src/crawling.ts
+++ src/crawling.ts
@@ -9,6 +9,7 @@ import { startDate } from './util/date';
 (function () {
   schedule.scheduleJob('0 5 0 * * * ', async () => {
     // schedule.scheduleJob('0 5 0 * * 1-7 ', async () => {
+    console.log('crawling');
     const mRawList: ImRaw[] = await storkService.getMrawList();
 
     const browser: Browser = await puppeteer.launch({
