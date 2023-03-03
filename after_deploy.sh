46e93e52d862b20c32e4a6bf015cff98c8fc3de9
yeongbin-gong <youngbin816@gmail.com>
2023-03-03 23:28:16 +0900

    puppeteer error

--- after_deploy.sh
+++ after_deploy.sh
@@ -1,6 +1,6 @@
 REPOSITORY=/home/ubuntu/st_backend
 cd $REPOSITORY
 
-npm install puppeteer --unsafe-perm
+npm install
 npm run typeorm migration:run -- -d ./dist/loaders/mysql.js
 pm2 start npm -- start
