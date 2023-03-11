6e53673a0b68b64f1248dfd6fb186054f929dac6
yeongbin-gong <youngbin816@gmail.com>
2023-03-11 16:29:37 +0900

    delete column

--- src/entities/log_5bill.ts
+++ src/entities/log_5bill.ts
@@ -19,8 +19,8 @@ export class log_5bill extends common {
   @Column({ name: 'c_updown_rate', type: 'bigint' })
   c_updown_rate: number;
 
-  @Column({ name: 'c_accum_volume', type: 'bigint' })
-  c_accum_volume: number;
+  // @Column({ name: 'c_accum_volume', type: 'bigint' })
+  // c_accum_volume: number;
 
   @Column({ name: 'c_accum_trans_price', type: 'bigint' })
   c_accum_trans_price: number;
@@ -37,35 +37,35 @@ export class log_5bill extends common {
   @Column({ name: 'c_lprice', type: 'bigint' })
   c_lprice: number;
 
-  @Column({ name: 'c_prev_com_sym', type: 'bigint' })
-  c_prev_com_sym: number;
+  // @Column({ name: 'c_prev_com_sym', type: 'bigint' })
+  // c_prev_com_sym: number;
 
-  @Column({ name: 'c_prev_trans_com_cnt', type: 'bigint' })
-  c_prev_trans_com_cnt: number;
+  // @Column({ name: 'c_prev_trans_com_cnt', type: 'bigint' })
+  // c_prev_trans_com_cnt: number;
 
-  @Column({ name: 'c_price_sell', type: 'bigint' })
-  c_price_sell: number;
+  // @Column({ name: 'c_price_sell', type: 'bigint' })
+  // c_price_sell: number;
 
-  @Column({ name: 'c_price_buy', type: 'bigint' })
-  c_price_buy: number;
+  // @Column({ name: 'c_price_buy', type: 'bigint' })
+  // c_price_buy: number;
 
-  @Column({ name: 'c_trans_price_inc', type: 'bigint' })
-  c_trans_price_inc: number;
+  // @Column({ name: 'c_trans_price_inc', type: 'bigint' })
+  // c_trans_price_inc: number;
 
-  @Column({ name: 'c_prev_trans_com_rat', type: 'float' })
-  c_prev_trans_com_rat: number;
+  // @Column({ name: 'c_prev_trans_com_rat', type: 'float' })
+  // c_prev_trans_com_rat: number;
 
-  @Column({ name: 'c_trans_rot', type: 'float' })
-  c_trans_rot: number;
+  // @Column({ name: 'c_trans_rot', type: 'float' })
+  // c_trans_rot: number;
 
-  @Column({ name: 'c_trans_price', type: 'bigint' })
-  c_trans_price: number;
+  // @Column({ name: 'c_trans_price', type: 'bigint' })
+  // c_trans_price: number;
 
   @Column({ name: 'c_power', type: 'float' })
   c_power: number;
 
-  @Column({ name: 'c_market_price', type: 'bigint' })
-  c_market_price: number;
+  // @Column({ name: 'c_market_price', type: 'bigint' })
+  // c_market_price: number;
 
   @Column({ name: 'b_price', type: 'bigint' })
   b_price: number;
--- src/models/cRaw.ts
+++ src/models/cRaw.ts
@@ -8,22 +8,22 @@ const cRawSchema = new mongoose.Schema(
     c_price: { type: Number }, //  체결가
     c_prev_com: { type: Number }, //  전일대비
     c_updown_rate: { type: Number }, //  등락율
-    c_accum_volume: { type: Number }, //  누적체결량
+    // c_accum_volume: { type: Number }, //  누적체결량
     c_accum_trans_price: { type: Number }, //  누적거래대금
     c_volume: { type: Number }, //  체결량
     c_sprice: { type: Number }, //  시가
     c_hprice: { type: Number }, //  고가
     c_lprice: { type: Number }, //  저가
-    c_prev_com_sym: { type: Number }, //  전일대비기호
-    c_prev_trans_com_cnt: { type: Number }, //  전일거래량대비(계약,주)
-    c_price_sell: { type: Number }, //  (최우선)매도호가
-    c_price_buy: { type: Number }, //  (최우선)매수호가
-    c_trans_price_inc: { type: Number }, //  거래대금증감
-    c_prev_trans_com_rat: { type: Number }, //  전일거래량대비(비율)
-    c_trans_rot: { type: Number }, //  거래회전율
-    c_trans_price: { type: Number }, //  거래비용
+    // c_prev_com_sym: { type: Number }, //  전일대비기호
+    // c_prev_trans_com_cnt: { type: Number }, //  전일거래량대비(계약,주)
+    // c_price_sell: { type: Number }, //  (최우선)매도호가
+    // c_price_buy: { type: Number }, //  (최우선)매수호가
+    // c_trans_price_inc: { type: Number }, //  거래대금증감
+    // c_prev_trans_com_rat: { type: Number }, //  전일거래량대비(비율)
+    // c_trans_rot: { type: Number }, //  거래회전율
+    // c_trans_price: { type: Number }, //  거래비용
     c_power: { type: Number }, //  체결강도
-    c_market_price: { type: Number }, //  시가총액
+    // c_market_price: { type: Number }, //  시가총액
   },
   {
     collection: 'c_raw',
